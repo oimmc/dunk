a4eaa4d0519663d3507cb4b264de2c5b8e509525
ommic <1045713269@qq.com>
2021-02-21 04:04:35 +0800

    feat: update src/dunk/Rebound.ts

---
+++ src/dunk/Rebound.ts
@@ -0,0 +1,69 @@
+import { requestAnimationFrame, cancelAnimationFrame } from '../utils'
+import Canvas from './Canvas'
+
+export interface ReboundsProps {
+	canvas: Canvas
+}
+
+export default class Rebounds {
+	canvas: Canvas
+	ctx: CanvasRenderingContext2D
+	x = 200
+	y = 50
+
+	constructor(props: ReboundsProps) {
+		const { canvas } = props
+		this.canvas = canvas
+		// this.canvas.drawLine(this.x, this.y)
+		this.ctx = this.canvas.getCtx()
+		this.draw()
+	}
+
+	draw() {
+		this.drawGround()
+		this.drawBoard()
+		this.drawBasketRing()
+	}
+
+	// 地面
+	drawGround() {
+		this.ctx.beginPath()
+		this.ctx.moveTo(0, 290)
+		this.ctx.lineTo(this.canvas.width, 280)
+		this.ctx.stroke()
+	}
+
+	drawBoard() {
+		this.ctx.save()
+		this.ctx.beginPath()
+
+		// 篮板
+		this.ctx.moveTo(180, 10)
+		this.ctx.lineTo(180, 80)
+		this.ctx.lineTo(280, 70)
+		this.ctx.lineTo(280, 0)
+		this.ctx.lineTo(180, 10)
+
+		// 准心框
+		this.ctx.moveTo(210, 65)
+		this.ctx.lineTo(252, 61)
+		this.ctx.lineTo(252, 28)
+		this.ctx.lineTo(210, 32)
+		this.ctx.lineTo(210, 65)
+		
+		// this.ctx.closePath()
+		this.ctx.stroke()
+		this.ctx.restore()
+	}
+
+	drawBasketRing() {
+		this.canvas.bezierElliptic(215, 60, 20, 4)
+	}
+
+	// update() {
+	// 	const update = () => {
+	// 		let animFrame = requestAnimationFrame(update)
+	// 	}
+	// 	update()
+	// }
+}
